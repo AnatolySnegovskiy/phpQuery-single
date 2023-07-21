9168dd827751cb4e98ecb26df62481063737c841
anatolii <CarrionGrow@Gmail.com>
2023-07-21 14:37:36 +0300

    test

--- Test/test.php
+++ Test/test.php
@@ -2,9 +2,19 @@
 
 include_once __DIR__ . '/../vendor/autoload.php';
 
-$pa = phpQuery::newDocument(file_get_contents(__DIR__ . '/test.html'));
+$html = <<<STR
+<div id="one">
+    <div class="two">
+        <a href="http://querylist.cc">QueryList官网</a>
+        <img src="http://querylist.cc/1.jpg" alt="这是图片">
+        <img src="http://querylist.cc/2.jpg" alt="这是图片2">
+    </div>
+    <span>其它的<b>一些</b>文本</span>
+</div>        
+STR;
 
-$pq = $pa->find('div:contains(":30")');
+$doc = phpQuery::newDocumentHTML($html);
 
-var_dump(pq($pq)->find('div')->eq(0)->text());
+$src = $doc->find('.two img:eq(0)')->attr('src');
+echo $src;
 
